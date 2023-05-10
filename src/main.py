4f359a179c43d944b24bafa143c1c6f51b507859
Juan Carlos <juan.sanchezp@udem.edu>
2023-05-09 21:14:47 -0600

    apply multiprocessing to use a gaussian filter

--- src/main.py
+++ src/main.py
@@ -1,12 +1,21 @@
 import numpy as np
 import cv2
 import argparse
+from multiprocessing import Pool, cpu_count
+
+def apply_gaussian_filter(frame):
+    # Apply a Gaussian filter with a kernel size of 5x5 and sigma value of 0
+    filtered_frame = cv2.GaussianBlur(frame, (5, 5), 0)
+    return filtered_frame
 
 parser = argparse.ArgumentParser(description='Vision-based object detection')
 parser.add_argument('--video_file', type=str, default='camera', help='Video file used for the object detection process')
 args = parser.parse_args()
 
 cv2.namedWindow('Video sequence',cv2.WINDOW_NORMAL)
+
+num_processes = cpu_count()
+pool = Pool(num_processes)
 
 cap=cv2.VideoCapture(args.video_file)
 
@@ -19,6 +28,11 @@ while(cap.isOpened()):
     if not ret:
         print("frame missed!")
         break
+
+    filtered_frame = pool.apply_async(apply_gaussian_filter, [frame])
+
+    # Display the filtered frame
+    cv2.imshow('Filtered Frame', filtered_frame.get())
 
     # Visualise the input video
     cv2.imshow('Video sequence',frame)
